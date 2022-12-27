98d2668bf55cb85737e0ec2322b754e2a0f8474a
wpeguero <wpeguerorosario@outlook.com>
2022-12-27 09:41:23 -0500

    CHANGE: Started with the development of the module
    Added the beginning surrounded by the module. code will be added later
    on. This is to begin writing the required skeleton framework for the
    entire project.

--- src/grove_cost_predictors.jl
+++ src/grove_cost_predictors.jl
@@ -1,3 +1,6 @@
 """
     Beginning of the file that will contain all of the imports required to activate the package.
 """
+module grove_cost_predictors
+    #This will contain all of the required imports
+end
