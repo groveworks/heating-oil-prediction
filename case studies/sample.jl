599e2348daae343b4ba9a0fcf47ae583e68f65f7
wpeguero <wpeguerorosario@outlook.com>
2023-01-13 13:46:45 -0500

    CHANGE:Added the previous value to features
    Added the previous row's oil price as one of the features required to
    predict the future price. Fixed InexactError where the number of samples
    taken for both training and testing where not a whole number.
    
    Started training the model based on 300 epochs, have also changed the
    overall model to further expand upon the features provided.

--- case studies/sample.jl
+++ case studies/sample.jl
@@ -2,6 +2,7 @@ using Plots
 using DataFrames
 using DelimitedFiles
 using ProgressMeter
+using Statistics
 using Dates
 using Flux
 using BSON: @save
@@ -26,44 +27,61 @@ df[!, :"\ufeffdate"] = Date.(df[!, :"\ufeffdate"], Dates.DateFormat("u d, yyyy")
 df[!, :"years"] = year.(df[!, :"\ufeffdate"])
 df[!, :"months"] = month.(df[!, :"\ufeffdate"])
 df[!, :"weeks"] = week.(df[!, :"\ufeffdate"])
+df[!, :"days"] = day.(df[!, :"\ufeffdate"])
 
+df[!, :weekly__dollars_per_gallon] = convert.(Float32, df[!, :weekly__dollars_per_gallon])
 df[!, :years] = convert.(Float32, df[!, :years])
 df[!, :months] = convert.(Float32, df[!, :months])
 df[!, :weeks] = convert.(Float32, df[!, :weeks])
+df[!, :days] = convert.(Float32, df[!, :days])
 
-X_train = Array(first(df[!, [:years, :months, :weeks]], Int(nrow(df) * 0.75)))'
-X_test = Array(last(df[!, [:years, :months, :weeks]], Int(nrow(df) * 0.25)))'
-y_train = Array(first(df[!, :weekly__dollars_per_gallon], Int(nrow(df) * 0.75)))'
-y_test = Array(last(df[!, :weekly__dollars_per_gallon], Int(nrow(df) * 0.25)))'
+df[!, :"pprice"] = missings(Float32, nrow(df))
+for (i, row) in enumerate(eachrow(df))
+    if i == 1
+        nothing
+    else
+        row["pprice"] = df[i-1, :weekly__dollars_per_gallon]
+    end
+end
+delete!(df, 1)
+df[!, :pprice] = convert.(Float32, df[!, :pprice])
+first(df, 10)
+X_train = Array(first(df[!, [:years, :months, :weeks, :days, :pprice]], Int(round(nrow(df) * 0.75))))'
+X_test = Array(last(df[!, [:years, :months, :weeks, :days, :pprice]], Int(round(nrow(df) * 0.25))))'
+y_train = Array(first(df[!, :weekly__dollars_per_gallon], Int(round(nrow(df) * 0.75))))'
+y_test = Array(last(df[!, :weekly__dollars_per_gallon], Int(round(nrow(df) * 0.25))))'
 train_loader = Flux.Data.DataLoader((X_train, y_train), shuffle=true);
-test_loader = Flux.Data.DataLoader((data=X_test, label=y_test), batchsize=64, shuffle=true);
+test_loader = Flux.Data.DataLoader((data=X_test, label=y_test), shuffle=true);
 #first(train_loader, 10)
 model = Chain(
-    GRU(3 => 5),
-    GRU(5 => 10),
+    GRU(5 => 20),
+    GRU(20 => 10),
     Dropout(0.3),
     GRU(10 => 2),
     Dense(2 => 1)
 )
 optim = Flux.setup(Adam(0.01), model)
 losses = []
-@showprogress for epoch in 1:100
+mean_losses = []
+@showprogress for epoch in 1:300
     for (x, y) in train_loader
         loss, grads = Flux.withgradient(model) do m
             # Evaluate model and loss inside gradient context:
             y_hat = m(x)
-            Flux.mae(y_hat, y)
+            Flux.mse(y_hat, y)
         end
         Flux.update!(optim, model, grads[1])
         push!(losses, loss)
     end
+    push!(mean_losses, mean(losses))
 end
 
 @save "mymodel.bson" model
 preds = model(X_test)
 acc = []
-for i in 1:length(preds)
+for i in eachindex(preds)
     a = 100 - ((abs(preds[i] - y_test[i])/y_test[i])*100)
     push!(acc, a)
 end
 mean(acc)
+plot(mean_losses)
