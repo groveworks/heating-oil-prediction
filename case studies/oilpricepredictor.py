81fe4ca382ce42c52f7f8d0394a241b8e445cb23
wpeguero <wpeguerorosario@outlook.com>
2023-01-21 14:48:51 -0500

    CHANGE:Started training the data.
    Set up the data in to the correct format for training the machine
    learning model in tensorflow. Currently, the accuracy stops at 53%
    regardless of batching or any other factor. This may be due to the
    learning rate decreasing faster than the loss.

--- case studies/oilpricepredictor.py
+++ case studies/oilpricepredictor.py
@@ -1,41 +1,42 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
+from tensorflow.keras import Sequential
 import pandas as pd
 import numpy as np
 
 def main():
-    df = pd.read_csv("B:/Documents/Github/grove-cost-predictors/data/heating oil prices.csv")
+    df = pd.read_csv("data/heating oil prices.csv")
     df['date'] = pd.to_datetime(df['date'])
     df['days'] = pd.DatetimeIndex(df['date']).day
     df['months'] = pd.DatetimeIndex(df['date']).month
     df['years'] = pd.DatetimeIndex(df['date']).year
     dff = df.drop(columns=['date'])
+    dff.to_csv("data/ctoilpricesfeats.csv", index=False)
     Xy = np.array(dff)
-    print(Xy.shape) # <-- work from there so that the shape of the data is reformatted in such a way that the previous price gets indexed
-    print(Xy)
-    exit()
-    for i, row in df.iterrows():
-        if i == 0:
-            pass
-        else:
-            df.at[i, 'previous_price'] = df.at[i-1, ['weekly__dollars_per_gallon', 'day', 'month', 'year']]
+    X = Xy[:, 1:4]
+    y = Xy[:,0]
+    look_back = 1
+    Xs, ys = [], []
+    for i in range(len(Xy) - look_back):
+        v = Xy[i:i+look_back]
+        Xs.append(v)
+        ys.append(Xy[i+look_back])
     # Design the machine learning algorithm
     model = SimpPredictor(4)
-    model.compile(optimizer='adam', loss=losses.MeanSquaredError(), metrics=['accuracy'])
-    y_train = df['weekly__dollars_per_gallon']
-    X_train = df.drop(columns=['weekly__dollars_per_gallon', 'date'])
-    dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).shuffle(600).batch(64)
-    model.fit(dataset, epochs=10)
+    model.compile(optimizer='Adam', loss='mse', metrics=['accuracy'])
+    dataset = tf.data.Dataset.from_tensor_slices((Xs, ys)).shuffle(600).batch(1)
+    model.fit(dataset, epochs=100)
 
 class SimpPredictor(tf.keras.models.Model):
     def __init__(self, in_dims):
         super().__init__(self)
         self.l1 = layers.GRU(in_dims, return_sequences=True)
-        self.l2 = layers.GRU(20)
+        self.l2 = layers.GRU(20, return_sequences=True)
         self.l3 = layers.Dropout(0.3)
-        self.l4 = layers.GRU(10)
-        self.l5 = layers.Dense(2)
+        self.l4 = layers.GRU(10, return_sequences=True)
+        self.l5 = layers.Dense(5)
+        self.l6 = layers.Dense(3)
         self.l6 = layers.Dense(1)
     
     def call(self, inputs):
