c0d80c116cc537a6b1c69b17e89f7c30b1a22fd4
wpeguero <wpeguerorosario@outlook.com>
2023-01-20 22:05:41 -0500

    CHANGE:Transformed data to numpy array
    Added portion that creates a numpy array from the pandas DataFrame
    because it is more difficult to transform the data as a pandas DataFrame
    than a numpy array.

--- case studies/oilpricepredictor.py
+++ case studies/oilpricepredictor.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 import pandas as pd
+import numpy as np
 
 def main():
     df = pd.read_csv("B:/Documents/Github/grove-cost-predictors/data/heating oil prices.csv")
@@ -9,11 +10,16 @@ def main():
     df['days'] = pd.DatetimeIndex(df['date']).day
     df['months'] = pd.DatetimeIndex(df['date']).month
     df['years'] = pd.DatetimeIndex(df['date']).year
+    dff = df.drop(columns=['date'])
+    Xy = np.array(dff)
+    print(Xy.shape) # <-- work from there so that the shape of the data is reformatted in such a way that the previous price gets indexed
+    print(Xy)
+    exit()
     for i, row in df.iterrows():
         if i == 0:
             pass
         else:
-            df.at[i, 'previous_price'] = df.at[i-1, 'weekly__dollars_per_gallon']
+            df.at[i, 'previous_price'] = df.at[i-1, ['weekly__dollars_per_gallon', 'day', 'month', 'year']]
     # Design the machine learning algorithm
     model = SimpPredictor(4)
     model.compile(optimizer='adam', loss=losses.MeanSquaredError(), metrics=['accuracy'])
